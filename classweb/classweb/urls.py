57e1dbea7964a6f5964a65e7d1f0a8c1e011c27b
Mohammad K Islam <62070498+kabirulislam95@users.noreply.github.com>
2021-12-11 15:32:04 -0500

    add unique id

--- classweb/classweb/urls.py
+++ classweb/classweb/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('about_course', views.about_course, name='about_course'),
     path('contact', views.contact, name='contact'),
     path('registration', views.registration, name='registration'),
+    path('unique_page', views.unique_page, name='unique_page'),
 
 ]
