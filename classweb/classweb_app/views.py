d41662f0c7e5e5b64aeacc311a3c0dc218754f53
Mohammad K Islam <62070498+kabirulislam95@users.noreply.github.com>
2021-12-11 01:31:59 -0500

    database added

--- classweb/classweb_app/views.py
+++ classweb/classweb_app/views.py
@@ -16,6 +16,23 @@ def about_course(request):
 def contact(request):
     return render(request, 'contact.html')
 
+
+def registration(request):
+
+    if request.method == 'POST':
+        first_name = request.POST ['first_name']
+        last_name = request.POST ['last_name']
+        email = request.POST ['email']  
+        username = request.POST['username'] 
+        password = request.POST ['password']
+        confirm_password = request.POST ['confirm_password']
+        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        user.save()
+        return render(request, 'loginpage.html')
+
+    else:
+        return render(request, 'registration.html')
+
 def loginpage(request):
     if request.method == 'POST':
         username = request.POST ['username']
