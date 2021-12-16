a1338fc2be0fae61dcb760c96ea185e1fb8a7a5f
Mohammad K Islam <62070498+kabirulislam95@users.noreply.github.com>
2021-12-16 02:44:03 -0500

    added all functionality

--- classweb/classweb_app/admin.py
+++ classweb/classweb_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from classweb_app.models import ProfessorUniqueId, StudentUniqueId, AllAssignment
 
-# Register your models here.
+#Register your models here.
 admin.site.register(StudentUniqueId)
 admin.site.register(ProfessorUniqueId)
 admin.site.register(AllAssignment)
